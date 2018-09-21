ec68f4118c32d11f5659b950473ace3d73a0fcbe
jzjy-li <jzyjzycz123>
2018-09-21 18:28:01 +0800

    node-min-server

--- node-min-server/example/server.js
+++ node-min-server/example/server.js
@@ -3,7 +3,10 @@ var minServer = require('../index')
 
 const options = {
    // static: './static',
-    static: './static',
+    static: {
+       'css':['./static'],
+       'js':['./static']
+    },
     port: '3000',
     proxy: {
         '/api': 'http:10.83.50.130:8080'  // 请求代理 旧：http:10.83.50.130:8080/getList 新 /api/getList
